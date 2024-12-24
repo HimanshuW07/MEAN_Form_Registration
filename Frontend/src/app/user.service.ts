006723b3aae898f627022ae373334466e938def5
HimanshuW07 <97220341+HimanshuW07@users.noreply.github.com>
2024-12-24 06:32:37 -0800

    Update user.service.ts

--- Frontend/src/app/user.service.ts
+++ Frontend/src/app/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:8080/users';
+  private apiUrl = 'https://mean-form-registration.onrender.com/users';
 
   constructor(private http: HttpClient) {}
 
